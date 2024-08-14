49a5867f18138f5a7dccf56d818160ca46793a81
Zellenon <zellenon@gmail.com>
2024-08-13 20:08:27 -0500

    fixed mutable egui bug

--- src/character_control_systems/mouse_grabbing.rs
+++ src/character_control_systems/mouse_grabbing.rs
@@ -2,19 +2,18 @@ use std::ops::Not;
 
 use bevy::{
     ecs::{
-        event::{Event, EventReader, EventWriter},
         query::With,
         system::{Query, Res, ResMut, Resource},
     },
     input::{keyboard::KeyCode, mouse::MouseButton, ButtonInput},
-    window::{CursorGrabMode, PrimaryWindow, Window},
+    window::{PrimaryWindow, Window},
 };
 
 #[derive(Resource)]
 pub struct MouseGrabbed(pub bool);
 
 pub(crate) fn sync_mouse_grab(
-    mut grab_resource: ResMut<MouseGrabbed>,
+    grab_resource: Res<MouseGrabbed>,
     mut primary_window_query: Query<&mut Window, With<PrimaryWindow>>,
 ) {
     let Ok(mut window) = primary_window_query.get_single_mut() else {
@@ -29,9 +28,8 @@ pub(crate) fn sync_mouse_grab(
     }
 }
 
-// TODO: eguicontexts shouldn't be mut, but for some reason the ctx() function doesn't exist??
 pub(crate) fn grab_mouse_on_click(
-    mut egui_context: bevy_egui::EguiContexts,
+    egui_context: bevy_egui::EguiContexts,
     mouse_buttons: Res<ButtonInput<MouseButton>>,
     primary_window_query: Query<&Window, With<PrimaryWindow>>,
     mut grab_resource: ResMut<MouseGrabbed>,
@@ -39,7 +37,7 @@ pub(crate) fn grab_mouse_on_click(
     if let Ok(window) = primary_window_query.get_single() {
         if window.cursor.visible
             && mouse_buttons.just_pressed(MouseButton::Left)
-            && egui_context.ctx_mut().is_pointer_over_area().not()
+            && egui_context.ctx().is_pointer_over_area().not()
         {
             grab_resource.0 = true;
             print!("Grabbing mouse!");
