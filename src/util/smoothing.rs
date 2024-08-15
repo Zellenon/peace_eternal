7c02f2e58d6f9b2296d1a4d127459976a9e716f4
Zellenon <zellenon@gmail.com>
2024-08-15 16:56:27 -0500

    updated smoothing to be time-based and added a rotation speed multiplier (mostly for guns)

--- src/util/smoothing.rs
+++ src/util/smoothing.rs
@@ -1,5 +1,6 @@
-use bevy::ecs::system::Query;
+use bevy::ecs::system::{Query, Res};
 use bevy::prelude::Transform;
+use bevy::time::Time;
 use bevy::{ecs::component::Component, reflect::Reflect};
 
 #[derive(Component, Reflect)]
@@ -9,6 +10,7 @@ pub struct SmoothedTransform {
     pub do_translate: bool,
     pub do_rotate: bool,
     pub do_scale: bool,
+    pub rotation_mul: f32,
 }
 
 impl Default for SmoothedTransform {
@@ -19,11 +21,12 @@ impl Default for SmoothedTransform {
             do_translate: false,
             do_rotate: false,
             do_scale: false,
+            rotation_mul: 1.,
         }
     }
 }
 
-pub fn smooth_movement(mut query: Query<(&mut Transform, &SmoothedTransform)>) {
+pub fn smooth_movement(time: Res<Time>, mut query: Query<(&mut Transform, &SmoothedTransform)>) {
     for (
         mut transform,
         &SmoothedTransform {
@@ -32,20 +35,25 @@ pub fn smooth_movement(mut query: Query<(&mut Transform, &SmoothedTransform)>) {
             do_translate,
             do_rotate,
             do_scale,
+            rotation_mul,
         },
     ) in query.iter_mut()
     {
         if do_translate {
             let delta = goal.translation - transform.translation;
-            transform.translation = transform.translation + delta * smoothing;
+            transform.translation =
+                transform.translation + delta * (smoothing * time.delta().as_secs_f32());
         }
         // TODO: Rotation smoothing isn't working well
         if do_rotate {
-            transform.rotation = transform.rotation.slerp(goal.rotation, smoothing);
+            transform.rotation = transform.rotation.slerp(
+                goal.rotation,
+                smoothing * time.delta().as_secs_f32() * rotation_mul,
+            );
         }
         if do_scale {
             let delta = goal.scale - transform.scale;
-            transform.scale = transform.scale + delta * smoothing;
+            transform.scale = transform.scale + delta * (smoothing * time.delta().as_secs_f32());
         }
     }
 }
