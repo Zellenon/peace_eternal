f618f1ebcce87c84a77ab2ea686f4fcfdca12e45
Zellenon <zellenon@gmail.com>
2024-08-16 03:21:06 -0500

    changed file exts to ogg, corrected "armor" typo

--- src/asset_setup/audio.rs
+++ src/asset_setup/audio.rs
@@ -6,73 +6,73 @@ use bevy_asset_loader::asset_collection::AssetCollection;
 
 #[derive(AssetCollection, Resource, Reflect)]
 pub struct PlaceholderAudio {
-    #[asset(path = "audio/placeholder/bodyimpact.wav")]
+    #[asset(path = "audio/placeholder/bodyimpact.ogg")]
     pub body_impact: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/boing_01.wav")]
+    #[asset(path = "audio/placeholder/boing_01.ogg")]
     pub boing: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/death_01.wav")]
+    #[asset(path = "audio/placeholder/death_01.ogg")]
     pub death1: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/death_02.wav")]
+    #[asset(path = "audio/placeholder/death_02.ogg")]
     pub death2: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/footstep_01.wav")]
+    #[asset(path = "audio/placeholder/footstep_01.ogg")]
     pub footstep1: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/footstep_02.wav")]
+    #[asset(path = "audio/placeholder/footstep_02.ogg")]
     pub footstep2: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/footstep_03.wav")]
+    #[asset(path = "audio/placeholder/footstep_03.ogg")]
     pub footstep3: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/footstep_04.wav")]
+    #[asset(path = "audio/placeholder/footstep_04.ogg")]
     pub footstep4: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/get_armor.wav")]
+    #[asset(path = "audio/placeholder/get_armour.ogg")]
     pub get_armor: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/gun_load_01.wav")]
+    #[asset(path = "audio/placeholder/gun_load_01.ogg")]
     pub gun_load1: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/gun_load_02.wav")]
+    #[asset(path = "audio/placeholder/gun_load_02.ogg")]
     pub gun_load2: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/gun_machine.wav")]
+    #[asset(path = "audio/placeholder/gun_machine.ogg")]
     pub machine_gun: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/gun_outofammo.wav")]
+    #[asset(path = "audio/placeholder/gun_outofammo.ogg")]
     pub gun_outofammo: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/gun_rifle_01.wav")]
+    #[asset(path = "audio/placeholder/gun_rifle_01.ogg")]
     pub rifle1: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/gun_rifle_02.wav")]
+    #[asset(path = "audio/placeholder/gun_rifle_02.ogg")]
     pub rifle2: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/gun_shot.wav")]
+    #[asset(path = "audio/placeholder/gun_shot.ogg")]
     pub gunshot: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/jump_01.wav")]
+    #[asset(path = "audio/placeholder/jump_01.ogg")]
     pub jump1: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/jump_02.wav")]
+    #[asset(path = "audio/placeholder/jump_02.ogg")]
     pub jump2: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/jump_03.wav")]
+    #[asset(path = "audio/placeholder/jump_03.ogg")]
     pub jump3: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/jump_04.wav")]
+    #[asset(path = "audio/placeholder/jump_04.ogg")]
     pub jump4: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/land.wav")]
+    #[asset(path = "audio/placeholder/land.ogg")]
     pub land: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/pain_01.wav")]
+    #[asset(path = "audio/placeholder/pain_01.ogg")]
     pub pain1: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/pain_02.wav")]
+    #[asset(path = "audio/placeholder/pain_02.ogg")]
     pub pain2: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/pain_03.wav")]
+    #[asset(path = "audio/placeholder/pain_03.ogg")]
     pub pain3: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/powerup_01.wav")]
+    #[asset(path = "audio/placeholder/powerup_01.ogg")]
     pub powerup1: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/powerup_02.wav")]
+    #[asset(path = "audio/placeholder/powerup_02.ogg")]
     pub powerup2: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/powerup_03.wav")]
+    #[asset(path = "audio/placeholder/powerup_03.ogg")]
     pub powerup3: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/powerup_04.wav")]
+    #[asset(path = "audio/placeholder/powerup_04.ogg")]
     pub powerup4: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/punch_01.wav")]
+    #[asset(path = "audio/placeholder/punch_01.ogg")]
     pub punch1: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/punch_02.wav")]
+    #[asset(path = "audio/placeholder/punch_02.ogg")]
     pub punch2: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/splash_01.wav")]
+    #[asset(path = "audio/placeholder/splash_01.ogg")]
     pub splash1: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/splash_02.wav")]
+    #[asset(path = "audio/placeholder/splash_02.ogg")]
     pub splash2: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/splash_03.wav")]
+    #[asset(path = "audio/placeholder/splash_03.ogg")]
     pub splash3: Handle<AudioSource>,
-    #[asset(path = "audio/placeholder/teleport.wav")]
+    #[asset(path = "audio/placeholder/teleport.ogg")]
     pub teleport: Handle<AudioSource>,
 }
 
