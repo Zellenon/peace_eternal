f6abd8961afd9ca1337696277c1dbf887701db55
Zellenon <zellenon@gmail.com>
2024-09-03 18:51:07 -0500

    added the first few guns

--- src/gameplay/content/guns.rs
+++ src/gameplay/content/guns.rs
@@ -1,22 +1,32 @@
 use bevy::prelude::Name;
 use bevy_composable::{app_impl::ComponentTreeable, tree::ComponentTree};
 
-use crate::gameplay::gunplay::{
-    guns::Gun,
-    servo::{FireMode, Servo},
-    servo_components::ShootsBullet,
+use crate::gameplay::{
+    gunplay::{
+        guns::Gun,
+        servo::{FireMode, Servo},
+        servo_components::ShootsBullet,
+    },
+    inventory::components::InventorySlotSize,
 };
 
-pub fn basic_gun() -> ComponentTree {
+pub fn basic_gun<T: Into<String>>(name: T) -> ComponentTree {
     (
-        Name::new("Gun"),
+        Name::new(name.into()),
         Gun,
         Servo {
             firemode: FireMode::SemiAuto,
             // cooldown: todo!(),
             ..Default::default()
         },
-        ShootsBullet,
     )
         .store()
+}
+
+pub fn pistol() -> ComponentTree {
+    basic_gun("Pistol") + (InventorySlotSize::Small).store()
+}
+
+pub fn autorifle() -> ComponentTree {
+    basic_gun("Auto Rifle") + (Servo::new(FireMode::FullAuto, 150)).store()
 }
