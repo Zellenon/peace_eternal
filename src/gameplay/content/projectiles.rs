aafea037e368e7dd9fcdeef18c53cb965ec92149
Zellenon <zellenon@gmail.com>
2024-09-03 23:16:45 -0500

    adjusted projectile defaults to account for new model size and also start with SpatialBundle

--- src/gameplay/content/projectiles.rs
+++ src/gameplay/content/projectiles.rs
@@ -1,5 +1,5 @@
 use avian3d::prelude::{Collider, RigidBody, SweptCcd};
-use bevy::prelude::Name;
+use bevy::prelude::{Name, SpatialBundle};
 use bevy_composable::{app_impl::ComponentTreeable, tree::ComponentTree};
 
 use crate::{
@@ -17,10 +17,11 @@ pub fn basic_bullet() -> ComponentTree {
             on_hit: ProjectileImpactBehavior::Die,
             on_impact: ProjectileImpactBehavior::Die,
         },
+        SpatialBundle::default(),
         SweptCcd::default(),
         RigidBody::Dynamic,
         Lifespan::default(),
-        Collider::sphere(0.5),
+        Collider::sphere(0.05),
         // Sensor,
         // TODO: damage
         LinkedModel::new(ModelResources::basic_bullet),
@@ -37,7 +38,7 @@ pub fn gl_projectile() -> ComponentTree {
         },
         RigidBody::Dynamic,
         Lifespan::new(10000),
-        Collider::sphere(0.5),
+        Collider::sphere(0.05),
         // TODO: damage
         // TODO:increased gravity
         // TODO: Grenade asset
